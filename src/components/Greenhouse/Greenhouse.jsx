dccdba4bb61cbc1fea91a69a6b173bd173ee0ad2
sam <sfriedman6711@gmail.com>
2024-02-08 16:08:11 -0700

    greenhouse partial phase one and two

--- src/components/Greenhouse/Greenhouse.jsx
+++ src/components/Greenhouse/Greenhouse.jsx
@@ -1,18 +1,16 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
-
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
+import { useTheme } from "../../context/ThemeContext";
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
 
 function Greenhouse() {
-
+  const { themeName, setThemeName } = useTheme();
+  console.log(themeName)
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
